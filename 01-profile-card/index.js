e9e21fbc584c8316d6cd5cb2f03c5ef07f21fe25
alik-balika <balikaalik@gmail.com>
2024-09-02 19:55:24 -0700

    Create my personal profile card

--- 01-profile-card/index.js
+++ 01-profile-card/index.js
@@ -1,1 +1,19 @@
-// Add your JavaScript here.
+document.addEventListener("DOMContentLoaded", () => {
+  const followButton = document.getElementById("follow-button");
+
+  followButton.addEventListener("click", () => {
+    if (followButton.innerText === "Follow") {
+      followButton.innerText = "Following";
+      followButton.classList.remove("bg-pink-400");
+      followButton.classList.add("bg-gray-400");
+      followButton.classList.remove("text-white");
+      followButton.classList.add("text-black");
+    } else {
+      followButton.innerText = "Follow";
+      followButton.classList.add("bg-pink-400");
+      followButton.classList.remove("bg-gray-400");
+      followButton.classList.add("text-white");
+      followButton.classList.remove("text-black");
+    }
+  });
+});
